62ee98d92b97a5f715701541e29b6113a6da892b
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-10 11:42:51 +0100

    adding the rest of the ex in 2.01 (calculator-one)

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -26,13 +26,31 @@
 
     document.getElementById("substraction").addEventListener("click", function() {
         // perform an substraction
+        let a = document.getElementById("op-one");
+        let b = document.getElementById("op-two");
+
+        let sub = Number(a.value)-Number(b.value); {
+            alert(sub);
+        }
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
         // perform an multiplication
+        let a = document.getElementById("op-one");
+        let b = document.getElementById("op-two");
+
+        let mul = Number(a.value)*Number(b.value); {
+            alert(mul);
+        }
     });
 
     document.getElementById("division").addEventListener("click", function() {
         // perform an division
+        let a = document.getElementById("op-one");
+        let b = document.getElementById("op-two");
+
+        let div = Number(a.value)/Number(b.value); {
+            alert(div);
+        }
     });
 })();
