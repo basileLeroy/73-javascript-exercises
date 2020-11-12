b6e7f06282b34ae73460be4507378dbb719a0def
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-12 10:22:58 +0100

    Adding new excersise -- finish 02-maths:05-factorial

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -9,14 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+
     
     // to get the value of an input: document.getElementById("element-id").value
 
     document.getElementById("run").addEventListener("click", function() {
 
-        // your code here
+        
+        let num = document.getElementById("number").value; // making a var (num) and linking it to the html input bar.
+        function x(num) { // creating a function (x) with the var (num).
+            if(num <= 1){
+                return num;
+            }
+            else {
+                return num * x(num - 1);
+            }
+        }
+        x(document.getElementById("number").value); 
+
+        alert(x(num)); // Be sure to call out the function WITH the var instead of only the var (num).
 
     });
 
-})();
+
