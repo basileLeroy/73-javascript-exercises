629ad0b5314944906c866822458018e7c5bba054
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-12 14:21:39 +0100

    Finishing 02-maths:06-fizzbuzz: needed to make a function name to use in the console.

--- 02-maths/06-fizzbuzz/script.js
+++ 02-maths/06-fizzbuzz/script.js
@@ -9,8 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+function number(i) {
+    
+        if (i > 100) { // added a message when exceeding the max value
+            console.log("You went too far...!");
+        }
+        
+            else if (i %3 === 0 && i %5 === 0) {
+                console.log(i + ' Fizzbuzz');
+            }
 
-    // your code here
+            else if (i %3 === 0) {
+                console.log(i + ' Fizz');
+                
+            }
+            else if (i%5 === 0) {
+                console.log(i + ' Buzz');
+            }
 
-})();
+            else (i <= 100); { // Max value for the standard message 
+                console.log(i);
+            }
+        
+};   
+
