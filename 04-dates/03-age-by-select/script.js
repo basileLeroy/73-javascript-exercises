fb0eb3a5185c723e9ca5cfe3b64ba414a3db563e
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-13 11:47:57 +0100

    finishing: 04-dates:03-age-by-select: made a groep of variables with the selected date, and another with the current date. if statements to mathematically take the difference and then using the getAge function to display the age

--- 04-dates/03-age-by-select/script.js
+++ 04-dates/03-age-by-select/script.js
@@ -9,8 +9,39 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+document.getElementById("run").addEventListener("click", function() {
+    
+
+    function getAge(birth) {
+        let day = document.getElementById("dob-day").value;
+        let month = document.getElementById("dob-month").value;
+        let year = document.getElementById("dob-year").value;
+    
+        const dob = day + ", " + month + ", " + year;
+    
+        let today = new Date();
+        let x = today.getDay();
+        let y = today.getMonth() + 1;
+        let z = today.getFullYear();
+        
+        console.log(dob);
+        
+
+        if (y == month && x >= day) return parseInt(z-year);
+        if (y == month && x < day) return parseInt(z-year-1);
+        if (y > month) return parseInt(z-year);
+        if (y < month) return parseInt(z-year-1); 
+     
+    }
+
+    let age = getAge("dob"); // asks for the const "dob", wich is displayed in in the same way as a date is.
+    alert(age);
+
+
+
+
+
+
 
 })();
