8a5244ee0ccb18463933e73b5dce75375095a73d
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-14 10:32:00 +0100

    finishing 06-objects:01-generate-object: made a const that will be displayed on click in the console log

--- 06-objects/01-generate-object/script.js
+++ 06-objects/01-generate-object/script.js
@@ -9,6 +9,15 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
+document.getElementById("run").addEventListener("click", function() {
+    const aboutMe = [
+        lastname = "Leroy",
+        firstname = "Basile",
+        age = "24",
+        city = "Gent",
+        Country = "chocolate-land",
+    ];
+
+    console.log(aboutMe);
     // your code here
-})();
+});
