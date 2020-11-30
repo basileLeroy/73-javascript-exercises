d40bed115b5246b9b3bd286d5997a807195c8151
Basile Leroy <basile.leroy.jnm@hotmail.com>
2020-11-30 11:20:27 +0100

    09-asynchronous: 01-get-post:
    finishing excersise

--- 09-asynchronous/01-get-posts/script.js
+++ 09-asynchronous/01-get-posts/script.js
@@ -12,4 +12,17 @@
 
 (() => {
     // your code here
+
+    // Making a function (function) on click (this is called a callback, as it reacts only when calling on it) 
+    document.querySelector('#run').addEventListener("click", function () {
+
+
+        // Adding new function inside the function (is also a variable) wit 2 parameters: error and articles
+        window.lib.getPosts(function (error, articles) {
+            console.log(error, articles);
+        });
+        
+        // the first and second parameters are set (in lib.getPost) and regognise what info to return inside those newly created parameters
+    });
+
 })();
